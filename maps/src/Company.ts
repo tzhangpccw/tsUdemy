72e3126d7328607f51ecccccd8dfa697cfeb1eed
tzhang <yuantiffanyzhang@gmail.com>
2024-01-12 20:07:24 +1000

    - map

---
+++ maps/src/Company.ts
@@ -0,0 +1,26 @@
+import { faker } from "@faker-js/faker";
+
+export class Company {
+    companyName: string;
+    catchPhrase: string;
+    location: {
+        lat: number,
+        lng: number
+    }
+    constructor() {
+        this.companyName = faker.company.name();
+        this.catchPhrase = faker.company.catchPhrase();
+        this.location = {
+            lat: faker.location.latitude(),
+            lng: faker.location.longitude()
+        }
+    }
+    markerContent(): string {
+        return `
+        <div>
+        <h1> Company Name: ${this.companyName} </h1>
+        <h3> catchPhrase: ${this.catchPhrase} </h3>
+        </div>
+        `;
+    }
+}
